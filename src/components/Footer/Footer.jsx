7ac6c137443b44cb623d6656031b3ad98e29a6ad
Djaypm89 <djaypm89@gmail.com>
2021-11-18 16:33:46 -0500

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer> className='footer'
+        <footer className='footer'>
             <h4>Copyright 2021</h4>
         </footer>
       );
